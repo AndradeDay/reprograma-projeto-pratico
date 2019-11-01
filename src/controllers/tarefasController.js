3480efd7542bd8f0ca22bcae8aea945f5439648c
AndradeDay <dayzalvz@gmail.com>
2019-10-31 22:11:30 -0300

    Coolocando rotas pra trazer tarefas por nome e tarefas concluidas

--- src/controllers/tarefasController.js
+++ src/controllers/tarefasController.js
@@ -8,8 +8,8 @@ exports.get = (req, res) => {
 
 exports.getTarefasId = (req, res) => {
     const id = req.params.id
-    if (id < 1 || id > tarefas.lenght) {
-        res.send("Tem Não")
+    if (id < 1 || id > tarefas.length) {
+        res.status(200).send("Mulher, Não tem")
     }
 
     const idTarefa = tarefas.find(idTarefa => idTarefa.id == id)
@@ -21,5 +21,15 @@ exports.getTarefasId = (req, res) => {
 exports.getNome = (req, res) => {
     const nome = req.params.nome
     const nomeTarefa = tarefas.filter(nomeTarefa => nomeTarefa.nome == nome)
-        res.status(200).send(nomeTarefa)
-    }
+
+    if (!nomeTarefa)
+        res.status(404).send("Que nome é essa fia?")
+
+    res.status(200).send(nomeTarefa)
+}
+
+
+exports.getConcluido = (req, res) => {
+    const tarefaConcluida = tarefas.filter(tarefaConcluida => tarefaConcluida.concluido === "true")
+    res.status(200).send(tarefaConcluida)
+ }
--- src/routes/tarefasRoutes.js
+++ src/routes/tarefasRoutes.js
@@ -5,5 +5,5 @@ const controller = require("../controllers/tarefasController")
 router.get("/", controller.get)
 router.get("/:id", controller.getTarefasId)
 router.get("/:nome/buscar", controller.getNome)
-
+router.get("/concluidos/filtrar", controller.getConcluido)
 module.exports = router;
